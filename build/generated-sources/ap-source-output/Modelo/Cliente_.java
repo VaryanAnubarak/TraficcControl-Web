372e21b5f6dc1967d7666cedd37ee68e67c24033
Varyan <varyananubarak@gmail.com>
2017-11-29 07:30:13 -0500

    [Code]Añadir lista vehiculos implementado

--- build/generated-sources/ap-source-output/Modelo/Cliente_.java
+++ build/generated-sources/ap-source-output/Modelo/Cliente_.java
@@ -1,11 +1,10 @@
 package Modelo;
 
-import Modelo.Vehiculo;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-28T19:23:10")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-29T07:21:46")
 @StaticMetamodel(Cliente.class)
 public class Cliente_ { 
 
@@ -15,7 +14,6 @@ public class Cliente_ {
     public static volatile SingularAttribute<Cliente, String> identificacion;
     public static volatile SingularAttribute<Cliente, Integer> tipoPago;
     public static volatile SingularAttribute<Cliente, String> pagoPeaje;
-    public static volatile SingularAttribute<Cliente, Vehiculo> vehiculo;
     public static volatile SingularAttribute<Cliente, String> nombre;
 
 }
